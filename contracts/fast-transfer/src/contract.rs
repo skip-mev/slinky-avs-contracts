b62db99ca6a62b4827aa34c8e10ceac2604d7e41
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2024-03-01 07:10:07 -0500

    fix clipy lint

--- contracts/fast-transfer/src/contract.rs
+++ contracts/fast-transfer/src/contract.rs
@@ -6,7 +6,9 @@ use cw2::set_contract_version;
 use osmosis_std::types::osmosis::tokenfactory::v1beta1::MsgCreateDenom;
 
 use crate::error::ContractError;
-use crate::execute::{execute_deposit, execute_slow_transfer, execute_withdraw, execute_fast_transfer};
+use crate::execute::{
+    execute_deposit, execute_fast_transfer, execute_slow_transfer, execute_withdraw,
+};
 use crate::helpers::{convert_to_assets, convert_to_shares};
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::query::query_vault_info;
@@ -63,9 +65,7 @@ pub fn execute(
         ExecuteMsg::SlowTransfer(transfer) => {
             execute_slow_transfer(deps, info, transfer.id, transfer.recipient)
         }
-        ExecuteMsg::FastTransfer(transfer) => {
-            execute_fast_transfer(deps, env, info, transfer)
-        }
+        ExecuteMsg::FastTransfer(transfer) => execute_fast_transfer(deps, env, info, transfer),
     }
 }
 
