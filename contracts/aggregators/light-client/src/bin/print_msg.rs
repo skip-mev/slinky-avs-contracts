3302b313801d3e141742b88e8b4640d9bb9d7bcd
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2024-02-29 23:06:33 -0500

    use aggregator package

--- contracts/aggregators/light-client/src/bin/print_msg.rs
+++ contracts/aggregators/light-client/src/bin/print_msg.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::schema_for;
-use schemars::_serde_json::{to_string_pretty};
-use slinky_avs_contracts::msg::{SudoMsg, Vote};
+use schemars::_serde_json::to_string_pretty;
+use slinky_avs_contracts::msg::Vote;
 
 fn main() {
     // let schema = schema_for!(SudoMsg);
